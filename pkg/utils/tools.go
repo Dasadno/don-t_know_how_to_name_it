b27df8897031f31e32f81ffdef80bbd5f3683ec0
dasadno <yakaleks96@gmail.com>
2026-03-12 18:47:41 +0500

    microservice is up

--- pkg/utils/tools.go
+++ pkg/utils/tools.go
@@ -1,5 +1,6 @@
 package utils
 
+// package for go mod tidy command
 import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
--- services/auth-service/cmd/main.go
+++ services/auth-service/cmd/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"context"
+
+	authv1 "github.com/Dasadno/service/server/gen/auth/v1"
+	rkboot "github.com/rookie-ninja/rk-boot/v2"
+	rkgrpc "github.com/rookie-ninja/rk-grpc/v2/boot"
+	"google.golang.org/grpc"
+)
+
+type AuthServer struct {
+	authv1.UnimplementedAuthServiceServer
+}
+
+func (s *AuthServer) Register(ctx context.Context, req *authv1.RegisterRequest) (*authv1.AuthResponse, error) {
+	return &authv1.AuthResponse{AccessToken: "fake-jwt"}, nil
+}
+
+func (s *AuthServer) Login(ctx context.Context, req *authv1.LoginRequest) (*authv1.AuthResponse, error) {
+	return &authv1.AuthResponse{AccessToken: "fake-jwt"}, nil
+}
+
+func main() {
+	ctx := context.WithoutCancel(context.Background())
+	boot := rkboot.NewBoot(rkboot.WithBootConfigPath("services/auth-service/boot.yaml", nil))
+	entry := rkgrpc.GetGrpcEntry("auth-service")
+
+	entry.AddRegFuncGrpc(func(server *grpc.Server) {
+		authv1.RegisterAuthServiceServer(server, &AuthServer{})
+	})
+	entry.AddRegFuncGw(authv1.RegisterAuthServiceHandlerFromEndpoint)
+
+	boot.Bootstrap(ctx)
+	boot.WaitForShutdownSig(ctx)
+}
